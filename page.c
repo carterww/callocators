d4e42d069441f8560443a87bdc6e69896a0fd4a3
carterww <carterww@hotmail.com>
2024-09-17 19:14:00 -0500

    bad code

--- page.c
+++ page.c
@@ -45,13 +45,40 @@ struct palloc_state state = {
 
 static void __c_die(const char *str);
 static void bootstrap_self();
+static void *__map_pages(size_t pnum);
 static int handle_mmap_fail(int err);
+
+// Not focusing on multithreading support just yet
+static int is_initialized = 0;
 
 void *palloc(size_t pnum)
 {
 	if (pnum == 0) {
 		errno = EINVAL;
 		return NULL;
+	}
+	if (!is_initialized) {
+		bootstrap_self();
+		is_initialized = 1;
+	}
+	assert(state.__pages != NULL);
+	struct __internal_page *curr_raw = state.__pages;
+	if (curr_raw->page_heads_cap == curr_raw->page_heads_num) {
+		// Need to pull from free list
+		void *raw_page = __map_pages(1);
+		struct __internal_page *rp = (struct __internal_page *)raw_page;
+		rp->page_heads_cap = (PAGE_SIZE - sizeof(*state.__pages)) /
+				     sizeof(*state.free_list);
+		rp->page_heads_num = 0;
+		state.__pages = rp;
+		struct __internal_page *tmp = curr_raw->next;
+		curr_raw->next = rp;
+		rp->prev = curr_raw;
+		rp->next = tmp;
+		if (curr_raw->prev == curr_raw) {
+			curr_raw->prev = rp;
+		}
+		curr_raw = rp;
 	}
 }
 
@@ -61,24 +88,23 @@ void pfree(void *pages);
 // Before we can
 static void bootstrap_self()
 {
-	// Allocate a single page with the following props:
-	// 1. Read/Write perms
-	// 2. Internal to the process
-	// 3. Anonymous page (not backed by file)
-	void *first_internal_page = mmap(NULL, PAGE_SIZE,
-					 PROT_READ | PROT_WRITE,
-					 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if (first_internal_page == MAP_FAILED) {
-		__c_die("Failed to boostrap page allocator");
-	}
+	void *raw_pages = __map_pages(2);
+	void *first_internal_page = raw_pages;
+	void *first_free_page = raw_pages + PAGE_SIZE;
 	struct __internal_page *fip =
 		(struct __internal_page *)first_internal_page;
+	struct palloc_page_head *free = &fip->pages[0];
+	free->prev = free;
+	free->next = free;
+	free->addr = first_free_page;
+	free->len = PAGE_SIZE;
 	fip->page_heads_cap =
 		(PAGE_SIZE - sizeof(*state.__pages)) / sizeof(*state.free_list);
-	fip->page_heads_num = 0;
+	fip->page_heads_num = 1;
 	fip->next = fip;
 	fip->prev = fip;
 	state.__pages = fip;
+	state.free_list = free;
 }
 
 // Figure out how we can handle the real ones?
@@ -101,6 +127,18 @@ static int handle_mmap_fail(int err)
 	case ENOMEM:
 		return 1;
 	}
+}
+
+static void *__map_pages(size_t pnum)
+{
+	assert(pnum != 0);
+	void *raw_pages = mmap(NULL, pnum * PAGE_SIZE, PROT_READ | PROT_WRITE,
+			       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	// Die for now on error
+	if (raw_pages == MAP_FAILED) {
+		__c_die("Failed to boostrap page allocator");
+	}
+	return raw_pages;
 }
 
 static void __c_die(const char *str)
